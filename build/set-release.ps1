4179ccf17ff5106503b2261cb666c2686085ca58
Petteri Stenius <petteri.stenius@gmail.com>
2024-09-19 09:49:24 +0300

    release 0.0.4

---
+++ build/set-release.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [version]
+    $Version
+)
+process {
+    $root = Split-Path $PSScriptRoot -Parent -Resolve -ErrorAction Stop
+    $psd = Join-Path $root "pwsh/PSUbi.TestModule/PSUbi.TestModule.psd1" -Resolve -ErrorAction Stop | Convert-Path
+    $csproj = Join-Path $root "cs/PSUbi.TestModule/PSUbi.TestModule.csproj" -Resolve -ErrorAction Stop | Convert-Path
+    
+    $null = Update-ModuleManifest -Path $psd -ModuleVersion $Version -ErrorAction Stop
+
+    $xml = [xml]::new()
+    $xml.PreserveWhitespace = $true
+    $null = $xml.Load($csproj)
+    $ProjectVersion = $xml.SelectSingleNode("/Project/PropertyGroup/Version")
+    $ProjectVersion.InnerText = $Version
+    $null = $xml.Save($csproj)
+
+    Write-Host -ForegroundColor Green @"
+git commit -a -m "release $($Version)"
+git tag "release/$($Version)"
+"@
+}
